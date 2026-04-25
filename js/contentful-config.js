6584263e7a53934c84fb68a823656a7a90c41806
Nick <nneedha@wgu.edu>
2026-04-25 14:31:29 -0500

    removed contenful api

--- js/contentful-config.js
+++ js/contentful-config.js
@@ -1,6 +1,6 @@
 export const CONTENTFUL = {
-  SPACE_ID: "fviougkbi52t",
+  SPACE_ID: "",
   ENVIRONMENT_ID: "master",
-  ACCESS_TOKEN: "-s8slzxy4UvY1VOFDR9gZwEWm3xWsTjIl21TWM7-8oA",
+  ACCESS_TOKEN: "",
   CONTENT_TYPE: "blogPost",
 };
